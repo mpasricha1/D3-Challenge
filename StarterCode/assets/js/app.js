4f7e89c63db50e11590304d7984e138b12262853
Mark Pasricha <markp862@gmail.com>
2020-08-21 08:25:12 -0400

    pushing

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,69 +1,58 @@
 // @TODO: YOUR CODE HERE!
+var svgWidth = 1000;
+var svgHeight = 1000;
 
-var margin = { top:20, right: 20, bottom: 30, left: 40}, 
-	width = 500 - margin.left - margin.right, 
-	height = 500 - margin.top - margin.bottom
+var margin = { top:30, right: 30, bottom: 30, left: 30}
+
+var chartWidth = svgWidth - margin.left - margin.right
+var	chartHeight = svgHeight = margin.top - margin.bottom
 	
 var svg = d3.select("#scatter")
 			.append("svg")
-				.attr("width", 500)
-				.attr("height", 500)
-			.append("g")
-				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			.attr("width", svgWidth)
+			.attr("height", svgHeight)
+				
+var chartGroup = svg.append("g")
+				.attr("transform", `translate(${margin.left}, ${margin.top})`);
 
 d3.csv("assets/data/data.csv").then(data => {
-	var stateInfo = { 
-		abbr: data.map(record => record.abbr),
-		hcare: data.map(record => record.healthcare),
-		poverty: data.map(record => record.poverty)
-	}
 
-	var x = d3.scaleLinear()
-			  .domain([6,25])
-			  .range([0,width])
-	svg.append("g")
-	   .attr("transform", "translate(0, " + height + ")")
-	   .call(d3.axisBottom(x))
+	data.forEach(d => {
+		d.poverty = +d.poverty
+		d.healthcare = +d.healthcare
+	})
 
-	var y = d3.scaleLinear()
-			  .domain([4,25])
-			  .range([height,0]);
-	svg.append("g")
-	   .call(d3.axisLeft(y)); 
+	console.log(data)
+	var xBandScale = d3.scaleBand()
+					   .domain(data.map(d => d.healthcare))
+					   .range([0, chartWidth])
+					   .padding(0.1)
 
-	svg.append('g')
-	   .selectAll("dot")
-	   .data(data)
-	   .enter()
-	   .append("circle")
-	   		.attr("cx", d => x(d.poverty))
-	   		.attr("cy", d => y(d.healthcare))
-	   		.attr("r", 6)
-	   		.style("fill", "#AED6F1")
+	var yLinearScale = d3.scaleLinear()
+						 .domain([0,d3.max(data, d => d.poverty)])
+						 .range([chartHeight, 0])
+
+	var bottomAxis = d3.axisBottom(xBandScale); 
+	var leftAxis = d3.axisLeft(yLinearScale); 
+
+	chartGroup.append("g")
+			  .call(leftAxis); 
+
+	chartGroup.append("g")
+			  .attr("transform", `translate(0, ${chartHeight})`)
+    		  .call(bottomAxis);
+
+    chartGroup.selectAll("#scatter")
+    		  .data(data)
+    		  .enter() 
+    		  .append("circle")
+    		  .attr("cx", d => xBandScale(d.povery))
+    		  .attr("cy", d => yLinearScale(d.healthcare))
+    		  .attr("width", xBandScale.bandwidth())
+    		  .attr("height", d => chartHeight - yLinearScale(d.povery))
 });
 
 
-	//Plotly version 
-	// var data = [{
-	// 	x: stateInfo.poverty,
-	// 	y: stateInfo.hcare,
-	// 	mode: "markers",
-	// 	type: "scatter"
-	// }];
 
-	// var layout = { 
-	// 	title: "Healthcare vs. Poverty", 
-	// 	xaxis: {
-	// 		title: { 
-	// 			text: "In Poverty(%)"
-	// 		}
-	// 	}, 
-	// 	yaxis: { 
-	// 		title: { 
-	// 			text: "Lacks Healthcare(%)"
-	// 		}
-	// 	}
-	// }
-	// Plotly.newPlot("scatter", data, layout) 
 
 	
